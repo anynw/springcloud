640800beb733374a9a016db14600f27a35658cf3
Wuhp <wuhuaping2006@126.com>
2019-09-24 00:05:40 +0800

    feign config

---
+++ microservicecloud/microservicecloud-api/src/main/java/com/ceair/springcloud/service/DeptClientService.java
@@ -0,0 +1,27 @@
+package com.ceair.springcloud.service;
+
+import java.util.List;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.ceair.springcloud.entities.Dept;
+/**
+ * 面向接口编程
+ * @author Administrator
+ * @date 2019年9月23日 下午11:04:35
+ */
+@FeignClient(value = "MICROSERVICECLOUD-DEPT")
+public interface DeptClientService {
+	
+	@RequestMapping(value = "/dept/get/{id}", method = RequestMethod.GET)
+	Dept get(@PathVariable("id") long id);
+
+	@RequestMapping(value = "/dept/list", method = RequestMethod.GET)
+	List<Dept> list();
+
+	@RequestMapping(value = "/dept/add", method = RequestMethod.POST)
+	boolean add(Dept dept);
+}
